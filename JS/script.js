57fd2d139af13cbb795ee71af85824aac405dc7c
Nick <er.nick@mail.ru>
2021-02-17 13:15:22 +0300

    cycles

--- JS/script.js
+++ JS/script.js
@@ -1,40 +1,24 @@
 "use strict";       //(директива) разработка проводится в современном режиме
 
-// if (4 == 4) {
-//     console.log('Ok!');
-// } else {
-//     console.log('Error');
+//Циклы
+
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
 // }
 
-
-
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Много');
-// } else {
-//     console.log('Ок!');
+// do {
+//     console.log(num);
+//     num++;
 // }
+// while (num < 55);
 
-// //Тернарный оператор
-
-// (num === 50) ? console.log('Ok!') : console.log('Error');
-
-const num = 50;
-
-//Конструкция switch упрощенное использования if и всегда идет на только строгое сравнение (===)
-
-switch (num) {
-    case 49: 
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('В точку!');
-        break;
-    default:
-        console.log('Не в это раз');
-        break;
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
 }
