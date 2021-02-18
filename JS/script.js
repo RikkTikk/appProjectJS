6d65f99be17e80d3027aa61af3666d9aa2b15600
Nick <er.nick@mail.ru>
2021-02-18 13:49:02 +0300

    practice 3 Ideal version

--- JS/script.js
+++ JS/script.js
@@ -1,49 +1,51 @@
 "use strict";       //(директива) разработка проводится в современном режиме
 
-let numberOfFilms;
-    
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-    }
-}
-
-start();
-
-function detectPersonalLevel() {
-    if (0<numberOfFilms && numberOfFilms<10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (9<numberOfFilms && numberOfFilms<30) {
-        console.log('Вы классический зритель');
-    } else if (29<numberOfFilms) {
-        console.log('Вы киноман!');
-    } else {
-        console.log('Что-то пошло не так');
-    }   
-}
-
-detectPersonalLevel();
-
 const personaMovieDB = {
-    count: numberOfFilms,
+    count: {},
     movies: {},
     actors: {},
     genvers: [],
     privat: false,
 };
 
-
-
-function rememberMyFilms () {
-    for (let i=0; i<2; i++) {
-        const lastNameFilm = prompt("Один из последних просмотренных фильмов?", ''),
-            points = +prompt("Насколько оцените его?", '');
-        if (lastNameFilm != null && lastNameFilm != '' && lastNameFilm.length < 50 && points != null && points != '') {
-            personaMovieDB.movies[lastNameFilm] = points;
+function start() {
+    for (let k = 0; k < 1; k++) {
+        const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');   
+        if (0<numberOfFilms && numberOfFilms<10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (9<numberOfFilms && numberOfFilms<30) {
+            console.log('Вы классический зритель');
+        } else if (29<numberOfFilms) {
+            console.log('Вы киноман!');
         } else {
+            alert('Что-то пошло не так');
+            k--;
+        }
+        personaMovieDB.count = numberOfFilms;
+    }
+}
+
+start();
+
+function rememberMyFilms() {
+    for (let i=0; i<2; i++) {
+        const lastNameFilm = prompt("Один из последних просмотренных фильмов?", '');
+        if (lastNameFilm == '' || lastNameFilm == null) {
+            alert("Поле не должно оставаться пустым!");
             i--;
+        } else if (lastNameFilm.length>50) {
+            alert("Название фильма не должно превышать 50 символов!");
+            i--;
+        } else {
+            for (let j = 0; j < 1; j++) {
+                const points = +prompt("Оцените просмотренный фильм", '');
+                if (points == '' || points == null || isNaN(points)) {
+                    alert('Поле не должно оставаться пустым!');
+                    j--;
+                } else{
+                    personaMovieDB.movies[lastNameFilm] = points;
+                }   
+            } 
         }
     }
 }
@@ -51,13 +53,13 @@ function rememberMyFilms () {
 rememberMyFilms();
 
 function writeYourGender() {
-    for (let i = 0; i < 3; i++) {
-        const likeGenvers = prompt("Какой Ваш любимый жанр фильмов?");
+    for (let i = 1; i < 4; i++) {
+        const likeGenvers = prompt(`Ваш любимый жанр под номером ${i}`);
         if (likeGenvers == '' || likeGenvers == null) {
             alert('Поле не должно оставаться пустым!');
             i--;
         } else {
-            personaMovieDB.genvers = likeGenvers;
+            personaMovieDB.genvers[i-1] = likeGenvers;
         }
     }
 }
