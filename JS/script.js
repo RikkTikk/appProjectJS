27ea14cb350616d05f22bab01c6940e569bcc334
Nick <er.nick@mail.ru>
2021-02-18 11:04:39 +0300

    practice 2, conditions & cycles

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,33 @@
 "use strict";       //(директива) разработка проводится в современном режиме
 
-//Циклы
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+    
+if (0<numberOfFilms && numberOfFilms<10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (9<numberOfFilms && numberOfFilms<30) {
+    console.log('Вы классический зритель');
+} else if (29<numberOfFilms) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Что-то пошло не так');
+}   
 
-let num = 50;
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genvers: [],
+    privat: false,
+};
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
+for (let i=0; i<2; i++) {
+    const lastNameFilm = prompt("Один из последних просмотренных фильмов?", ''),
+        points = +prompt("Насколько оцените его?", '');
+    if (lastNameFilm != null && lastNameFilm != '' && lastNameFilm.length < 50 && points != null && points != '') {
+        personaMovieDB.movies[lastNameFilm] = points;
+    } else {
+        i--;
     }
-    console.log(i);
 }
+
+console.log(personaMovieDB);
