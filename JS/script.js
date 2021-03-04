48d06d8a21b0c5cd6aee37a3132a9adc955f6ecb
Nick <er.nick@mail.ru>
2021-03-04 11:55:18 +0300

    dynamic typisation

--- JS/script.js
+++ JS/script.js
@@ -1,86 +1,59 @@
 "use strict";       //(директива) разработка проводится в современном режиме
 
-const personalMovieDB = {
-    count: {},
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start: function() {
-        for (let k = 0; k < 1; k++) {
-            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');   
-            if (0<personalMovieDB.count && personalMovieDB.count<10) {
-                console.log('Просмотрено довольно мало фильмов');
-            } else if (9<personalMovieDB.count && personalMovieDB.count<30) {
-                console.log('Вы классический зритель');
-            } else if (29<personalMovieDB.count) {
-                console.log('Вы киноман!');
-            } else {
-                alert('Введите число!');
-                k--;
-            }
-        }
-    },
-    rememberMyFilms: function() {
-        for (let i=0; i<2; i++) {
-            personalMovieDB.movies = prompt("Один из последних просмотренных фильмов?", '');
-            if (personalMovieDB.movies == '' || personalMovieDB.movies == null) {
-                alert("Поле не должно оставаться пустым!");
-                i--;
-            } else if (personalMovieDB.movies.length>50) {
-                alert("Название фильма не должно превышать 50 символов!");
-                i--;
-            } else {
-                for (let j = 0; j < 1; j++) {
-                    const points = +prompt("Оцените просмотренный фильм", '');
-                    if (points == '' || points == null || isNaN(points)) {
-                        alert('Поле не должно оставаться пустым!');
-                        j--;
-                    } else{
-                        personalMovieDB.movies = points;
-                    }   
-                } 
-            }
-        }
-    },
-    showMyDB: function (hidden) {
-        if (!hidden) {
-            console.log(personalMovieDB);
-        }
-    },
-    toggleVisibleMyDB: function() {
-        if (personalMovieDB.privat) {
-            personalMovieDB.privat = false;
-        } else {
-            personalMovieDB.privat = true;
-        }
-    },
-    writeYourGenres: function() {
-        for (let i = 1; i < 4; i++) {
-            personalMovieDB.genres.push(prompt(`Введите ваши любимые жанры через пробел`).toLowerCase());
-            if (personalMovieDB.genres == ' ' || personalMovieDB.genres == null) {
-                alert('Поле не должно оставаться пустым!');
-                i--;
-            } else {
-                personalMovieDB.genres.sort();
-            } 
-        }
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Любимый жанр #${i + 1} - это ${item}`);
-        });
-    }       
-};
+//Динамическая типизация - возможность одного типа данных превращаться в другой
 
-    // for (let i = 1; i <= 3; i++) {
-    //         let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-            // if (genre == '' || genre == null) {
-            //     alert('Поле не должно оставаться пустым!');
-            //     i--;
-            // } else {
-            //     personalMovieDB.genres[i - 1] = genre;
-            // } 
-    //     }
-    //     personalMovieDB.genres.forEach((item, i) => {
-    //         console.log(`Любимый жанр #${i + 1} - это ${item}`);
-    //     });
-    // }
+//To string
+
+//1)
+
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+//2)
+console.log(typeof(null + ''));
+
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num);       //Старая версия написания кода(сейчас исп интерполяция)
+
+const fontSize = 26 + "px";
+
+//To Number
+
+//1)
+
+console.log(typeof(Number('4')));       //Уже не исполльзуют
+
+//2)
+
+console.log(typeof(+('5')));        //Современное исполнение через унарный +
+
+//3)
+console.log(typeof(parseInt('15px', 10)));
+
+
+let answer = +prompt("Hello", "");
+
+//To Boolean
+
+//1)
+
+// 0, '', null, undefined, NaN === false
+
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+//2)
+console.log(typeof(Boolean('4')));
+
+//3)
+console.log(typeof(!!"44444"));
